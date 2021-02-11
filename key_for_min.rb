347ce5420ab9f4e93898c77e7acb344ebe2778b7
darryl_williams <darrylwilliams6@gmail.com>
2021-02-11 18:24:49 -0500

    commit tree f7f7fef5371b0b18f83064669fb6b0ed2077466d

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#method takes in argument of hash, create new array to assign values after collecting hash
 def key_for_min_value(name_hash)
-
+    array = []
+    name_hash.each do |person, value|
+        array << value
+    end
+    lowest_value = array[0]
+    array.each do |i|
+        if i < lowest_value
+            lowest_value = i
+        end
+    end
+    name_hash.each do |person, value|
+        if value == lowest_value
+        return person
+      end
+    end
+    return nil if name_hash = {}
 end
